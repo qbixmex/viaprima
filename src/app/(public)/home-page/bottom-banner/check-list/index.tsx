5484b994c2eec7e9986313f6abc650f5c6a90d5f
Daniel González <qbixmex@gmail.com>
2026-09-18 15:54:25 -0600

    Home page improvements

--- src/app/(public)/home-page/bottom-banner/check-list/index.tsx
+++ src/app/(public)/home-page/bottom-banner/check-list/index.tsx
@@ -1,16 +1,17 @@
 import type { FC } from "react";
 import { Icon } from "@iconify/react";
 import { checkList } from "./data";
+import styles from "./styles.module.css";
 
 export const CheckList: FC = () => {
   return (
-    <>
+    <div className={styles.distributionCheckList}>
       {checkList.map((item) => (
-        <div key={item} className="flex items-center gap-2.5">
-          <Icon icon="mdi:check-circle" className="text-blue-400 text-[20px]" />
+        <div key={item} className={styles.listItem}>
+          <Icon icon="mdi:check-circle" className={styles.icon} />
           <span>{item}</span>
         </div>
       ))}
-    </>
+    </div>
   );
 };
